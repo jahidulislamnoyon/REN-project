4c43ec6a9d313e53bd75353a4587f43f979b5698
jahidulislamnoyon <152014755+jahidulislamnoyon@users.noreply.github.com>
2024-10-30 19:58:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// //$(selector).action()
+
+// $(document).ready(function(){
+
+//     $(window).scroll(function(){
+
+//         if($(window).scrollTop() > 0) {
+
+//             $(".futter").addCless("sticky");
+
+//         }else{
+//             $(".futter").removeClass("sticky");
+//         }
+
+//     });
+// });
